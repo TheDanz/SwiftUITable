2dcc9e0be902126a334fca77decf1ddd319e4987
Danil Ryumin <ryumin_danil@icloud.com>
2022-06-06 13:51:19 +0300

    Initial Commit

---
+++ SwiftUITable/SwiftUITableApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITableApp.swift
+//  SwiftUITable
+//
+//  Created by Danil Ryumin on 06.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
